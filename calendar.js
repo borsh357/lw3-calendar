3f40f39d7aa4ef2fc3c71ca5d751026d283db85a
borsh357 <alexey.cher357@gmail.com>
2019-10-04 12:22:57 +0300

    changed styles and js

--- calendar.js
+++ calendar.js
@@ -1,20 +1,9 @@
 var calendarThemeSelector = document.getElementById("calendar-theme-selector");
 calendarThemeSelector.onchange = function() {
   var themeSelectorValue = calendarThemeSelector.value;
-  var elementsArray = document.getElementsByClassName('calendar-item');
   if (themeSelectorValue == "Dark") {
-    for (var i = 0; i < elementsArray.length; i++) {
-      elementsArray[i].classList.add('calendar-dark');
-      document.getElementsByClassName('calendar-month')[0].classList.add('calendar-dark');
-      document.getElementsByClassName('calendar-block')[0].classList.add('calendar-dark');
-      document.getElementsByClassName('calendar-head')[0].classList.add('calendar-dark');
-    }
+    document.getElementsByClassName('calendar')[0].classList.add('calendar-dark');
   } else {
-    for (var i = 0; i < elementsArray.length; i++) {
-      elementsArray[i].classList.remove('calendar-dark');
-      document.getElementsByClassName('calendar-month')[0].classList.remove('calendar-dark');
-      document.getElementsByClassName('calendar-block')[0].classList.remove('calendar-dark');
-      document.getElementsByClassName('calendar-head')[0].classList.remove('calendar-dark');
-    }
+    document.getElementsByClassName('calendar')[0].classList.remove('calendar-dark');
   }
 }
